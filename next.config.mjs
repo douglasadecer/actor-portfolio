2f49a3575765fe124fb10dd47137709d0c47c6c3
Douglas Adecer <douglasadecer@y7mail.com>
2025-06-18 23:41:55 +1000

    Redirect root to /youtube

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // For Static Export
-  // output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   optimizeFonts: false,
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/youtube',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
