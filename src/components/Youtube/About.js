d767a65405fb9df290a79c0e495a23cfe6041c7a
Douglas Adecer <douglasadecer@y7mail.com>
2025-06-19 23:37:36 +1000

    updated About.js

--- src/components/Youtube/About.js
+++ src/components/Youtube/About.js
@@ -65,7 +65,7 @@ const About = () => {
                       </li>
                       <li>
                         Phone:
-                        <span> 278 057 406 16+</span>
+                        <span> 278 057 406 61+</span>
                       </li>
                     </ul>
                   </div>
